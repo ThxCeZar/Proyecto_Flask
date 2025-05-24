bc1767e8c3d143a59ec4acdf947383ba69e42b6a
ThxCeZar <thecesargamer39@gmail.com>
2025-05-24 16:51:01 -0500

    Actualización v2.1.0.1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return render_template('index.html')
+    return render_template('/templates/PaginaInicioSes.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
